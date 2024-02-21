aa1a1905e24f07de7a1fe9fb97b0b462a0a14df2
harshittsaini <harshitsaini61@gmail.com>
2024-02-21 11:53:49 +0530

    created navbar

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div >
-      <Navbar/>
 
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn} />} />
         <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
+
     </div>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import Template from "../components/Template";
+import loginImg from "../assets/login.png";
 
-const Login = () => {
+function Login({ setIsLoggedIn }) {
   return (
-    <div>Login</div>
-  )
+    <Template
+      title="Welcome Back"
+      description1="Build skills for today, tomorrow, and beyond."
+      description2="Education to future-proof your career."
+      image={loginImg}
+      formType="login"
+      setIsLoggedIn={setIsLoggedIn}
+    />
+  );
 }
 
-export default Login
+export default Login;
